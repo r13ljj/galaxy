341ec0a604af5d4baef20f521741874e99ff10b8
r13ljj@gmail.com <r13ljj@gmail.com>
2018-03-26 01:09:23 +0800

    add balance

---
+++ rpc/cluster/src/main/java/com/jonex/dss/cluster/loadbalance/ConsistentHashLoadBalance.java
@@ -0,0 +1,17 @@
+package com.jonex.dss.cluster.loadbalance;
+
+import com.jonex.dss.rpc.Provider;
+import com.jonex.dss.rpc.Supplier;
+
+import java.util.List;
+
+/**
+ * Created by xubai on 2018/03/26 上午1:08.
+ */
+public class ConsistentHashLoadBalance extends AbstractLoadBalance {
+
+    @Override
+    protected Provider doSelect(List<Provider> providerList, Supplier supplier) {
+        return null;
+    }
+}
