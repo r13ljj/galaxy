4fbc84f0f5a4217142154a5119b02e2f79a1133e
lijunjun <lijunjun@globalegrow.com>
2018-05-04 13:47:53 +0800

    add netty

--- remoting/src/test/java/bio/BioSocketServer.java
+++ remoting/src/test/java/bio/BioSocketServer.java
@@ -1,5 +1,11 @@
 package bio;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * <pre>
  *
@@ -17,4 +23,16 @@ package bio;
  * </pre>
  */
 public class BioSocketServer {
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(9999);
+            System.out.println("客户端启动成功");
+            Socket socket = serverSocket.accept();
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
