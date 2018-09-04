2a6afce2f2bfb0c07d505cb3e4978bc367d2340b
r13ljj@gmail.com <r13ljj@gmail.com>
2018-09-04 15:32:32 +0800

    add input

--- remoting/src/test/java/bio/BioSocketClient.java
+++ remoting/src/test/java/bio/BioSocketClient.java
@@ -29,8 +29,8 @@ public class BioSocketClient {
     public static void main(String[] args) throws IOException {
         try {
             // 1、创建客户端Socket，指定服务器地址和端口
-            // Socket socket=new Socket("127.0.0.1",5200);
-            Socket socket = new Socket("10.40.2.135", 12248);
+            Socket socket=new Socket("127.0.0.1",9999);
+            //Socket socket = new Socket("10.40.2.135", 12248);
             System.out.println("客户端启动成功");
             // 2、获取输出流，向服务器端发送信息
             // 向本机的52000端口发出客户请求
--- remoting/src/test/java/bio/BioSocketServer.java
+++ remoting/src/test/java/bio/BioSocketServer.java
@@ -3,6 +3,7 @@ package bio;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -27,9 +28,19 @@ public class BioSocketServer {
     public static void main(String[] args) {
         try {
             ServerSocket serverSocket = new ServerSocket(9999);
-            System.out.println("客户端启动成功");
+            System.out.println("服务端启动成功");
             Socket socket = serverSocket.accept();
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            // 由系统标准输入设备构造BufferedReader对象
+            PrintWriter write = new PrintWriter(System.out);
+            String msg = in.readLine();
+            while(msg != null && !"end".equals(msg)){
+                write.println("server receive:"+msg);
+                write.flush();
+                msg=in.readLine();
+            }
+            in.close();
+            socket.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
